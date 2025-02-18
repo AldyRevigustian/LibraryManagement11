743d09da3d2d19fe50333b513aa0af59e90bdb59
Aldy Revigustian <aldybudiasih@gmail.com>
2025-02-18 15:42:39 +0700

    Change Auth Table

---
+++ app/Models/Admin.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Admin extends Authenticatable
+{
+
+    protected $table = 'admins';
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,25 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
+        Admin::create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make("Admin123"),
         ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        //     'password' => Hash::make("Aldey"),
+        // ]);
     }
 }
