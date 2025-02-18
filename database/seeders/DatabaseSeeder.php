47c9572aa7606f0347b84d90c9b082312b674b66
Aldy Revigustian <aldybudiasih@gmail.com>
2025-02-18 15:52:05 +0700

    Remove Unused Model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Admin;
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -15,12 +14,7 @@ class DatabaseSeeder extends Seeder
         Admin::create([
             'name' => 'Test User',
             'email' => 'admin@admin.com',
-            'password' => Hash::make("Admin123"),
+            'password' => Hash::make("admin"),
         ]);
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        //     'password' => Hash::make("Aldey"),
-        // ]);
     }
 }
