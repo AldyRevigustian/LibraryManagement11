49e73faf946c2224dd09e0437498a2a325310296
AldyRevigustian <aldyrevig@gmail.com>
2025-03-18 22:21:07 +0700

    Welcome Page

--- resources/views/guest/welcome.blade.php
+++ resources/views/guest/welcome.blade.php
@@ -183,10 +183,10 @@
 
 @section('content')
     <div class="container">
-        <div class="mt-4"
+        <div class="mt-5"
             style="height: 250px; width: 1140px; background-image: url('{{ asset('assets/images/banner.png') }}'); background-size: cover; background-position: center; border-radius: 10px; display: flex; justify-content: center; align-items: center; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.05);">
         </div>
-        <div class="rekomendasi mt-5">
+        <div class="rekomendasi mt-4">
             <div style="display: flex; flex-direction: row; justify-content: space-between; align-items: flex-end;">
                 <div class="section-header">
                     <h5 class="text-start mb-0">Rekomendasi Buku</h5>
